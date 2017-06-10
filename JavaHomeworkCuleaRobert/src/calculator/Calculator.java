b7e07b7adeb650b883d204b2118c7e29a9af214b
Robert Culea <>
2017-06-10 13:43:18 +0300

    A doua versiune de calculator

--- JavaHomeworkCuleaRobert/src/calculator/Calculator.java
+++ JavaHomeworkCuleaRobert/src/calculator/Calculator.java
@@ -16,6 +16,7 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Test github");
     }
     
 }
